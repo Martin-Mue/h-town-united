470a9f4c994f1fa3cc5be02ddf753a2f4a965614
Martin-Mue <martin.mueller3788@yahoo.de>
2026-08-10 23:04:43 +0200

    changes for logo

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,6 @@ const App = () => (
             <Route path="/auth" element={<Auth />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/live/:slug" element={<PublicTournament />} />
-            <Route path="/__index-preview" element={<Index />} />
             <Route
               path="/*"
               element={
